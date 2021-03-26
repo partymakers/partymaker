ac84d88f5896b91872bdc0f10e6eeba121145f23
Karyna <karina.babenko16@gmail.com>
2021-03-26 02:49:19 +0100

    Open MainActivity on buttonLogin click

--- app/src/main/java/com/github/partymakers/partymaker/activities/LoginActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/activities/LoginActivity.java
@@ -3,7 +3,9 @@ package com.github.partymakers.partymaker.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import com.github.partymakers.partymaker.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void loginSuccessful(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
