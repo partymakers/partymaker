d302dc2e14e9f8578d16a3870516f68d0301200c
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-03-28 00:49:32 +0100

    Add FirebaseUI for authentication handling

--- app/src/main/java/com/github/partymakers/partymaker/MainActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/MainActivity.java
@@ -1,23 +1,81 @@
 package com.github.partymakers.partymaker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.github.partymakers.partymaker.user.LoginActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    static final private String TAG = MainActivity.class.getSimpleName();
+    static final private int REQUEST_CODE_LOGIN = 1;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(true){ // change condition: if the user is not logged in
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        if (user == null) {
+            crateLoginIntent();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        replace with switch if you're adding more cases
+        if (requestCode == REQUEST_CODE_LOGIN) {
+            handleLoginResult(resultCode, data);
+        }
+    }
+
+    private void crateLoginIntent() {
+        List<AuthUI.IdpConfig> loginProviders = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(loginProviders)
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                REQUEST_CODE_LOGIN
+        );
+    }
+
+    private void handleLoginResult(int resultCode, Intent intent) {
+        IdpResponse response = IdpResponse.fromResultIntent(intent);
+        if (resultCode == RESULT_OK) {
+            Log.i(TAG, "Login successful");
+            user = firebaseAuth.getCurrentUser();
+//                TODO handle
+        } else if (resultCode == RESULT_CANCELED || response == null) {
+            Log.w(TAG, "Login canceled");
+//                TODO handle
+        } else {
+            String message = "Login failed";
+            if (response != null) {
+                int errorCode = response.getError().getErrorCode();
+                message += ". Error code: " + errorCode;
+            }
+            Log.e(TAG, message);
+//                TODO handle
         }
     }
 }
