9f244b74f4879d3d5dfac03ea009574068141c72
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-05-02 22:48:35 +0200

    Implement data binding for all fields in party creation layout besides time, date, food and drinks

--- app/src/main/java/com/github/partymakers/partymaker/party/CreatePartyActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/party/CreatePartyActivity.java
@@ -27,7 +27,6 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -40,8 +39,8 @@ public class CreatePartyActivity extends AppCompatActivity implements View.OnCli
     static private final List<String> tagListDrinks = Arrays.asList("Soda", "Light beer", "Craft beer", "Cocktail", "Juice", "Soft drinks", "Vodka", "Whiskey", "Martini", "Shots", "Wine", "Tea", "Coffee");
 
     private ActivityCreatePartyBinding dataBinding;
-    private CollectionReference partiesRepo;
-    private final PartyEntity party = new PartyEntity();
+    private final CollectionReference partiesRepo = FirebaseFirestore.getInstance().collection("parties");
+    private PartyEntity party;
 
     // TODO: text input check and set errors https://codelabs.developers.google.com/codelabs/mdc-111-kotlin/#2
     @Override
@@ -50,243 +49,25 @@ public class CreatePartyActivity extends AppCompatActivity implements View.OnCli
 
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_create_party);
         setContentView(dataBinding.getRoot());
-
-        partiesRepo = FirebaseFirestore.getInstance().collection("parties");
-        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        party.getOrganizersIds().add(userId);
-        party.getParticipantsIds().add(userId);
+        if (getIntent().hasExtra("party")) {
+            party = getIntent().getParcelableExtra("party");
+        } else {
+            party = new PartyEntity();
+            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            party.getOrganizersIds().add(userId);
+            party.getParticipantsIds().add(userId);
+        }
+        dataBinding.setParty(party);
 
         setTag(tagListFood, party.getFood(), dataBinding.foodChipGroup);
         setTag(tagListDrinks, party.getDrinks(), dataBinding.drinksChipGroup);
 
-
-        dataBinding.textPartyName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setName(dataBinding.textPartyName.getText().toString());
-            }
-        });
-        dataBinding.textPartyDescription.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setDescription(dataBinding.textPartyDescription.getText().toString());
-            }
-        });
         dataBinding.textDatePicked.setOnClickListener(this);
         dataBinding.textTimePicked.setOnClickListener(this);
-        dataBinding.textLocationPicked.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setLocation(dataBinding.textLocationPicked.getText().toString());
-            }
-        });
-        dataBinding.textInputTheme.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setTheme(dataBinding.textInputTheme.getText().toString());
-            }
-        });
-        dataBinding.textInputDressCode.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setDressCode(dataBinding.textInputDressCode.getText().toString());
-            }
-        });
         dataBinding.foodChipButton.setOnClickListener(this);
         dataBinding.drinksChipButton.setOnClickListener(this);
-        dataBinding.textInputFee.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    parseFee();
-                }
-            }
-        });
-        dataBinding.checkBoxPartner.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                party.setAllowsPartner(isChecked);
-            }
-        });
-
-        dataBinding.checkBoxFriends.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                party.setAllowsFriends(isChecked);
-            }
-        });
-        dataBinding.checkBoxChildren.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                party.setAllowsChildren(isChecked);
-            }
-        });
-        dataBinding.checkBoxPets.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                party.setAllowsPets(isChecked);
-            }
-        });
-        dataBinding.textInputParking.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setParkingDetails(dataBinding.textInputParking.getText().toString());
-            }
-        });
-        dataBinding.textInputInfo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                party.setAdditionalInformation(dataBinding.textInputInfo.getText().toString());
-            }
-        });
         dataBinding.submitButton.setOnClickListener(this);
-
-        dataBinding.switchDressCode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.dressCodeInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputDressCode.setVisibility(View.VISIBLE);
-                    party.setDressCode(dataBinding.textInputDressCode.getText().toString());
-                } else {
-                    // If the switch button is off
-                    dataBinding.dressCodeInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputDressCode.setVisibility(View.GONE);
-                    party.setDressCode(null);
-                }
-            }
-
-        });
-
-        dataBinding.switchTheme.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.themeInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputTheme.setVisibility(View.VISIBLE);
-                    party.setTheme(dataBinding.textInputTheme.getText().toString());
-                } else {
-                    // If the switch button is off
-                    dataBinding.themeInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputTheme.setVisibility(View.GONE);
-                    party.setTheme(null);
-                }
-            }
-
-        });
-
-        dataBinding.switchFood.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.foodChipGroup.setVisibility(View.VISIBLE);
-                    dataBinding.foodInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputFood.setVisibility(View.VISIBLE);
-                    dataBinding.foodChipButton.setVisibility(View.VISIBLE);
-                    dataBinding.foodChipInputLayout.setVisibility(View.VISIBLE);
-                    for (int i = 0; i < dataBinding.foodChipGroup.getChildCount(); i++) {
-                        Chip chip = (Chip) dataBinding.foodChipGroup.getChildAt(i);
-                        if (chip.isChecked()) {
-                            party.getFood().add(chip.getText().toString());
-                        }
-                    }
-                } else {
-                    // If the switch button is off
-                    dataBinding.foodChipGroup.setVisibility(View.GONE);
-                    dataBinding.foodInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputFood.setVisibility(View.GONE);
-                    dataBinding.foodChipButton.setVisibility(View.GONE);
-                    dataBinding.foodChipInputLayout.setVisibility(View.GONE);
-                    party.getFood().clear();
-                }
-            }
-
-        });
-
-        dataBinding.switchDrinks.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.drinksChipGroup.setVisibility(View.VISIBLE);
-                    dataBinding.drinksInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputDrinks.setVisibility(View.VISIBLE);
-                    dataBinding.drinksChipButton.setVisibility(View.VISIBLE);
-                    dataBinding.drinksChipInputLayout.setVisibility(View.VISIBLE);
-                    for (int i = 0; i < dataBinding.drinksChipGroup.getChildCount(); i++) {
-                        Chip chip = (Chip) dataBinding.drinksChipGroup.getChildAt(i);
-                        if (chip.isChecked()) {
-                            party.getDrinks().add(chip.getText().toString());
-                        }
-                    }
-                } else {
-                    // If the switch button is off
-                    dataBinding.drinksChipGroup.setVisibility(View.GONE);
-                    dataBinding.drinksInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputDrinks.setVisibility(View.GONE);
-                    dataBinding.drinksChipButton.setVisibility(View.GONE);
-                    dataBinding.drinksChipInputLayout.setVisibility(View.GONE);
-                    party.getDrinks().clear();
-                }
-            }
-
-        });
-
-        dataBinding.switchFee.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.feeLinearLayout.setVisibility(View.VISIBLE);
-                    dataBinding.feeInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputFee.setVisibility(View.VISIBLE);
-                    parseFee();
-                } else {
-                    // If the switch button is off
-                    dataBinding.feeLinearLayout.setVisibility(View.GONE);
-                    dataBinding.feeInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputFee.setVisibility(View.GONE);
-                    party.setFee(null);
-                }
-            }
-
-        });
-
-        dataBinding.switchParking.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // If the switch button is on
-                    dataBinding.parkingInputLayout.setVisibility(View.VISIBLE);
-                    dataBinding.textInputParking.setVisibility(View.VISIBLE);
-                    party.setParkingDetails(buttonView.getText().toString());
-                } else {
-                    // If the switch button is off
-                    dataBinding.parkingInputLayout.setVisibility(View.GONE);
-                    dataBinding.textInputParking.setVisibility(View.GONE);
-                    party.setParkingDetails(null);
-                }
-            }
-
-        });
-
     }
-
-    private void parseFee() {
-        String amountString = dataBinding.textInputFee.getText().toString();
-        try {
-            BigDecimal amount = new BigDecimal(amountString);
-            party.setFee(amount);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e(TAG, "invalid fee");
-            party.setFee(null);
-        }
-    }
-
 
     //onclick listeners for date/time pickers, food/... chip input
     @Override
