77bea19651dd76027de3dc74b7bd903ebebf142b
Karyna <karina.babenko16@gmail.com>
2021-05-04 03:09:42 +0200

    Setup ViewPartyActivity

--- app/src/main/java/com/github/partymakers/partymaker/party/ViewPartyActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/party/ViewPartyActivity.java
@@ -8,14 +8,42 @@ import android.os.Bundle;
 import com.github.partymakers.partymaker.R;
 import com.github.partymakers.partymaker.databinding.ActivityViewPartyBinding;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
 public class ViewPartyActivity extends AppCompatActivity {
     private ActivityViewPartyBinding dataBinding;
+
+    PartyEntity party = new PartyEntity();
+
+    {
+        party.setName("Dope af");
+        party.setDescription("Long loooooooooong description");
+        party.setTimestamp(new Date().getTime());
+        party.setLocation("location");
+        party.setTheme("theme");
+        party.setDressCode("dress code");
+        party.setFood(Arrays.asList("Pizza", "Kebab", "Saszety"));
+        party.setDrinks(Arrays.asList("Vodka", "Tea", "void juice"));
+        party.setFee("miljon cebulionów");
+        party.setAllowsPartner(true);
+        party.setParkingDetails("just walk, duh");
+        party.setAdditionalInformation("grass is green");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_view_party);
         setContentView(dataBinding.getRoot());
+        dataBinding.setParty(party);
 
+        if (party.getTimestamp() != null) {
+            String dateTime = SimpleDateFormat.getDateTimeInstance(DateFormat.FULL, 2).format(new Date(party.getTimestamp()));
+            dataBinding.textViewPartyDateTime.setText("When: " + dateTime);
+        }
     }
 }
