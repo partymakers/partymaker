fe1e45a8807158d2d205d3aa090f588bf3b4e674
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-05-04 04:59:57 +0200

    Move MainActivity and DashboardFragment logic to ViewModel and migrate them do databinding

--- app/src/main/java/com/github/partymakers/partymaker/MainActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/MainActivity.java
@@ -2,21 +2,19 @@ package com.github.partymakers.partymaker;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.github.partymakers.partymaker.databinding.ActivityMainBinding;
 import com.github.partymakers.partymaker.dashboard.SectionsPagerAdapter;
+import com.github.partymakers.partymaker.databinding.ActivityMainBinding;
+import com.github.partymakers.partymaker.user.UserViewModel;
 import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,33 +23,24 @@ public class MainActivity extends AppCompatActivity {
     static final private String TAG = MainActivity.class.getSimpleName();
     static final private int REQUEST_CODE_LOGIN = 1;
 
-    private FirebaseAuth firebaseAuth;
-    private FirebaseUser user;
-
+    private UserViewModel userViewModel;
     private ActivityMainBinding dataBinding;
-    private Bundle userInfo = new Bundle();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        if (!userViewModel.isLoggedIn()) {
+            createLoginIntent();
+        }
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = dataBinding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = dataBinding.tabs;
         tabs.setupWithViewPager(viewPager);
-
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        user = firebaseAuth.getCurrentUser();
-        if (user == null) {
-            createLoginIntent();
-        } else {
-            userInfo.putString("name", user.getDisplayName());
-            userInfo.putString("email", user.getEmail());
-        }
     }
 
     @Override
@@ -80,30 +69,8 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
-    // TODO: login status setter
     private void handleLoginResult(int resultCode, Intent intent) {
         IdpResponse response = IdpResponse.fromResultIntent(intent);
-        if (resultCode == RESULT_OK) {
-            Log.i(TAG, "Login successful");
-            user = firebaseAuth.getCurrentUser();
-            userInfo.putString("name", user.getDisplayName());
-            userInfo.putString("email", user.getEmail());
-            userInfo.putString("status", "Login successful");
-        } else if (resultCode == RESULT_CANCELED || response == null) {
-            Log.w(TAG, "Login cancelled");
-            userInfo.putString("status", "Login cancelled");
-        } else {
-            String message = "Login failed";
-            if (response != null) {
-                int errorCode = response.getError().getErrorCode();
-                message += ". Error code: " + errorCode;
-            }
-            Log.e(TAG, message);
-            userInfo.putString("status", message);
-        }
-    }
-
-    public Bundle getUserInfo() {
-        return userInfo;
+        userViewModel.handleAuthUiResult(resultCode, response, TAG);
     }
 }
