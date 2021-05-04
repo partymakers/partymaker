73b7fc263b9cd597bc9d46046e2166889b005c5c
Karyna <karina.babenko16@gmail.com>
2021-05-04 04:19:25 +0200

    Polish ViewPartyUI

--- app/src/main/java/com/github/partymakers/partymaker/party/ViewPartyActivity.java
+++ app/src/main/java/com/github/partymakers/partymaker/party/ViewPartyActivity.java
@@ -12,7 +12,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.Locale;
 
 public class ViewPartyActivity extends AppCompatActivity {
     private ActivityViewPartyBinding dataBinding;
@@ -40,6 +39,20 @@ public class ViewPartyActivity extends AppCompatActivity {
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_view_party);
         setContentView(dataBinding.getRoot());
         dataBinding.setParty(party);
+
+
+//for the future (add "intent.putExtra("type", "edit");" to DashboardFragment.java - join button onclick)
+//        Bundle bundle = getIntent().getExtras();
+//
+//        if (bundle.getString("type").equals("join")) {
+//            dataBinding.imageButtonViewPartyEdit.setVisibility(View.GONE);
+//            dataBinding.buttonsViewParty.setVisibility(View.VISIBLE);
+//            //dataBinding.foodJoinInputLayout.setVisibility(View.VISIBLE);
+//            //dataBinding.infoViewPartyInputLayout.setVisibility(View.VISIBLE);
+//        } else if (bundle.getString("type").equals("edit")) {
+//            dataBinding.imageButtonViewPartyEdit.setVisibility(View.VISIBLE);
+//            dataBinding.buttonsViewParty.setVisibility(View.GONE);
+//        }
 
         if (party.getTimestamp() != null) {
             String dateTime = SimpleDateFormat.getDateTimeInstance(DateFormat.FULL, 2).format(new Date(party.getTimestamp()));
