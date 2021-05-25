76599a111ead048229da84f16b8a0f9e7e825446
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-05-25 04:29:03 +0200

    Add Firebase user modification methods in UserRepository

--- app/src/main/java/club/partymaker/partymaker/user/UserRepository.java
+++ app/src/main/java/club/partymaker/partymaker/user/UserRepository.java
@@ -1,12 +1,22 @@
 package club.partymaker.partymaker.user;
 
+import android.app.Activity;
 import android.content.Intent;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.OAuthProvider;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +28,7 @@ import club.partymaker.partymaker.R;
 
 @Singleton
 public class UserRepository {
+    private final LiveData<FirebaseUser> firebaseUser;
     private final List<AuthUI.IdpConfig> loginProviders = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build()
@@ -26,10 +37,13 @@ public class UserRepository {
 
     @Inject
     public UserRepository() {
+        MutableLiveData<FirebaseUser> mutableFirebaseUser = new MutableLiveData<>();
         MutableLiveData<String> mutableUserId = new MutableLiveData<>();
         FirebaseAuth.getInstance().addAuthStateListener(firebaseAuth -> {
+            mutableFirebaseUser.setValue(firebaseAuth.getCurrentUser());
             mutableUserId.setValue(firebaseAuth.getUid());
         });
+        this.firebaseUser = mutableFirebaseUser;
         this.userId = mutableUserId;
     }
 
@@ -48,5 +62,58 @@ public class UserRepository {
 
     public String getUserIdValue() {
         return userId.getValue();
+    }
+
+    public void updateDisplayedName(String name, OnCompleteListener<Void> onCompleteListener) {
+        FirebaseUser user = firebaseUser.getValue();
+        if (user == null) {
+            return;
+        }
+        UserProfileChangeRequest changeRequest = new UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .build();
+        user.updateProfile(changeRequest)
+                .addOnCompleteListener(onCompleteListener);
+    }
+
+    public void updateEmail(String password, String newEmail,
+                            OnFailureListener onAuthenticationFailureListener,
+                            OnCompleteListener<Void> onUpdatingEmailListener) {
+        FirebaseUser user = firebaseUser.getValue();
+        if (user != null && user.getProviderId().equals(EmailAuthProvider.PROVIDER_ID)) {
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), password);
+
+            user.reauthenticate(credential)
+                    .addOnFailureListener(onAuthenticationFailureListener)
+                    .addOnSuccessListener(unused -> {
+                        user.verifyBeforeUpdateEmail(newEmail).addOnCompleteListener(onUpdatingEmailListener);
+                    });
+        }
+    }
+
+    public void updatePassword(String oldPassword, String newPassword,
+                               OnFailureListener onAuthenticationFailureListener,
+                               OnCompleteListener<Void> onUpdatingPasswordListener) {
+        FirebaseUser user = firebaseUser.getValue();
+        if (user != null && user.getProviderId().equals(EmailAuthProvider.PROVIDER_ID)) {
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldPassword);
+
+            user.reauthenticate(credential)
+                    .addOnFailureListener(onAuthenticationFailureListener)
+                    .addOnSuccessListener(unused -> {
+                        user.updatePassword(newPassword).addOnCompleteListener(onUpdatingPasswordListener);
+                    });
+        }
+    }
+
+    public void linkWithGoogle(Activity activity, Runnable onSuccessListener, OnFailureListener onFailureListener) {
+        FirebaseUser user = firebaseUser.getValue();
+        String googleAuthId = GoogleAuthProvider.PROVIDER_ID;
+        if (user != null && !user.getProviderId().equals(googleAuthId)) {
+            OAuthProvider provider = OAuthProvider.newBuilder(googleAuthId).build();
+            user.startActivityForLinkWithProvider(activity, provider)
+                    .addOnSuccessListener(activity, authResult -> onSuccessListener.run())
+                    .addOnFailureListener(activity, onFailureListener);
+        }
     }
 }
